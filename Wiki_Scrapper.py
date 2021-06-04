e44bef9d2c65b99f2a78fb517b7a578aee221374
sandeep <sandy4gautam@gmail.com>
2021-06-04 09:37:59 +0530

    Third commit
    added comments and cleaned the code

--- Wiki_Scrapper.py
+++ Wiki_Scrapper.py
@@ -1,15 +1,24 @@
+#imports
 import requests
 from bs4 import BeautifulSoup
 import random
-def myscrapper(url):
-	response = requests.get(
-		url=url,
-	)
+
+#functiuon to scrap wikipedia page titles randomly
+def wikiscrapper(url):
+	#requesting a response by passing the url
+	response = requests.get(url=url,)
+	#parsing the html content
 	soup = BeautifulSoup(response.content,'html.parser')
+	#getting heading from the webpage
 	title = soup.find(id='firstHeading')
+	#printing the title
 	print(title.text)
+	#getting all the links from the current webpage
 	all_links = soup.find(id='bodyContent').find_all('a')
+	#empty list to save only links that have /wiki/ in them and not https links
+	#since we only want wikipedia links
 	new_links = []
+	#for loop to iterate through links and save only wikipedia links in list new_links
 	for link in all_links:
 		try:
 			if link['href'].find('/wiki/') != -1:
@@ -18,10 +27,11 @@ def myscrapper(url):
 		except KeyError:
 			continue
 
-	random.shuffle(new_links)
-
+	#getting a random link from the list
 	linkToScrape = random.choice(new_links)
 
-	myscrapper("https://en.wikipedia.org" + linkToScrape['href'])
+	#opening this new randomly selected link and repeating the process again
+	wikiscrapper("https://en.wikipedia.org" + linkToScrape['href'])
 
-myscrapper("https://en.wikipedia.org/wiki/Crypto")
+#calling the function
+wikiscrapper("https://en.wikipedia.org/wiki/Crypto")
